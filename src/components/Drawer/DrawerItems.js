287b2fde917d1f598386b7a382dbc5c75eb83283
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-03-08 17:16:11 -0600

    Create DrawerScreen component file

--- src/components/Drawer/DrawerItems.js
+++ src/components/Drawer/DrawerItems.js
@@ -1,36 +1,10 @@
 import { StyleSheet } from 'react-native'
-import { router, usePathname } from 'expo-router'
-import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer'
+import { DrawerContentScrollView } from '@react-navigation/drawer'
 import { useTranslation } from 'react-i18next'
 import { DrawerHeader } from './DrawerHeader'
+import { DrawerScreen } from './DrawerScreen'
 import { Category, Pencil, Settings } from '@/icons'
-import { colors, fonts } from '@/constants'
-
-export function DrawerScreen({ label, icon, path }) {
-    const pathname = usePathname()
-
-    return (
-        <DrawerItem
-            label={label}
-            style={styles.item}
-            labelStyle={styles.label}
-            activeTintColor={colors.primary}
-            inactiveTintColor={colors.text}
-            activeBackgroundColor={colors.primary15}
-            focused={pathname === path}
-            onPress={() => {
-                router.push(path)
-            }}
-            icon={({ color, size }) => (
-                icon({
-                    width: size,
-                    height: size,
-                    color
-                })
-            )}
-        />
-    )
-}
+import { colors } from '@/constants'
 
 export function DrawerItems() {
     const { t } = useTranslation()
@@ -61,14 +35,5 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: colors.background,
-    },
-    item: {
-        borderRadius: 16,
-        paddingVertical: 8,
-        paddingHorizontal: 16,
-    },
-    label: {
-        fontFamily: fonts.mono,
-        textTransform: 'uppercase',
     },
 })
---
+++ src/components/Drawer/DrawerScreen.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from 'react-native'
+import { usePathname } from 'expo-router'
+import { DrawerItem } from '@react-navigation/drawer'
+import { colors, fonts } from '@/constants'
+
+export function DrawerScreen({ label, icon, path }) {
+    const pathname = usePathname()
+
+    return (
+        <DrawerItem
+            label={label}
+            style={styles.item}
+            labelStyle={styles.label}
+            activeTintColor={colors.primary}
+            inactiveTintColor={colors.text}
+            activeBackgroundColor={colors.primary15}
+            focused={pathname === path}
+            onPress={() => {
+                router.push(path)
+            }}
+            icon={({ color, size }) => (
+                icon({
+                    width: size,
+                    height: size,
+                    color
+                })
+            )}
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+    item: {
+        borderRadius: 16,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    label: {
+        fontFamily: fonts.mono,
+        textTransform: 'uppercase',
+    },
+})
