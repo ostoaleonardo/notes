d95e91f19aa356f0763ea3b66e0013d197f97e65
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-03-17 17:57:10 -0600

    The password must be at least 4 characters

--- src/components/ModalChildrens/PasswordModal.js
+++ src/components/ModalChildrens/PasswordModal.js
@@ -2,15 +2,18 @@ import { useEffect, useState } from 'react'
 import * as Crypto from 'expo-crypto'
 import { StyleSheet, View } from 'react-native'
 import { useTranslation } from 'react-i18next'
+import * as Animatable from 'react-native-animatable'
 import { ModalSheet } from '../Modal'
+import { Typography } from '../Text'
 import { PasswordInput } from '../Input'
 import { Button } from '../Button'
+import { colors } from '@/constants'
 
 export function PasswordModal({ isVisible, onClose, handlePassword }) {
     const { t } = useTranslation()
-    const [password, setPassword] = useState('')
     const [passwordInput, setPasswordInput] = useState('')
-    const [isButtonDisabled, setIsButtonDisabled] = useState(false)
+    const [encryptedPassword, setEncryptedPassword] = useState('')
+    const [isInvalidPassword, setIsInvalidPassword] = useState(false)
 
     useEffect(() => {
         (async () => {
@@ -18,9 +21,18 @@ export function PasswordModal({ isVisible, onClose, handlePassword }) {
                 Crypto.CryptoDigestAlgorithm.SHA256,
                 passwordInput
             )
-            setPassword(digest)
+            setEncryptedPassword(digest)
         })()
     }, [passwordInput])
+
+    const checkPassword = () => {
+        if (passwordInput.length < 4) {
+            setIsInvalidPassword(true)
+            return
+        }
+
+        handlePassword(encryptedPassword)
+    }
 
     return (
         <ModalSheet
@@ -29,15 +41,26 @@ export function PasswordModal({ isVisible, onClose, handlePassword }) {
             title={t('title.password')}
         >
             <View style={styles.container}>
-                <PasswordInput
-                    autoFocus
-                    password={passwordInput}
-                    onChangeText={setPasswordInput}
-                />
+                <View style={styles.inputContainer}>
+                    <Animatable.View animation={isInvalidPassword ? 'shake' : undefined}>
+                        <PasswordInput
+                            autoFocus
+                            password={passwordInput}
+                            onChangeText={setPasswordInput}
+                            onChange={() => setIsInvalidPassword(false)}
+                        />
+                    </Animatable.View>
+                    <Typography
+                        variant='caption'
+                        textAlign='center'
+                        color={colors.primary}
+                    >
+                        {isInvalidPassword && t('messages.lengthPassword')}
+                    </Typography>
+                </View>
                 <Button
-                    disabled={isButtonDisabled}
+                    onPress={checkPassword}
                     label={t('buttons.save')}
-                    onPress={() => handlePassword(password)}
                 />
             </View>
         </ModalSheet>
@@ -48,8 +71,12 @@ const styles = StyleSheet.create({
     container: {
         width: '100%',
         gap: 40,
-        paddingVertical: 24,
+        paddingVertical: 8,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    inputContainer: {
+        width: '100%',
+        alignItems: 'center',
     },
 })
