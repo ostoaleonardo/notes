056841df253ef0c12938e6d167c40d7bb5b7573b
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-03-18 16:18:44 -0600

    Update constants

---
+++ src/constants/googlePlay.js
@@ -0,0 +1,1 @@
+export const GOOGLE_PLAY_URL = 'https://play.google.com/store/apps/details?id=com.monospace.notes'
--- src/constants/playStore.js
+++
@@ -1,1 +0,0 @@
-export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.monospace.notes'
