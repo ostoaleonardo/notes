45f329dc7ab4dc37ba64a1e198cf044a26ad168d
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-03-19 17:07:36 -0600

    Hide the content of the note when it has a password

--- src/components/SwipeableCard/SwipeableNote.js
+++ src/components/SwipeableCard/SwipeableNote.js
@@ -7,6 +7,7 @@ import { Typography } from '../Text'
 import { useNotes } from '@/hooks'
 import { getDimensions } from '@/utils'
 import { colors } from '@/constants'
+import { Lock } from '@/icons'
 
 export function SwipeableNote({ id, title, note, images, hasPassword }) {
     const { deleteNote } = useNotes()
@@ -36,30 +37,56 @@ export function SwipeableNote({ id, title, note, images, hasPassword }) {
             >
                 <Pressable
                     onPress={goToEdit}
-                    style={styles.noteContainer}
+                    style={styles.pressableContainer}
                 >
-                    <Typography
-                        uppercase
-                    >
-                        {title}
-                    </Typography>
-                    <Typography>
-                        {note}
-                    </Typography>
-                    {images.length > 0 && (
-                        <View style={styles.noteImages}>
-                            {images.map((image, index) => (
-                                <Image
-                                    key={index}
-                                    source={{ uri: image }}
-                                    style={[styles.noteImage, {
-                                        width: width + '%',
-                                        height: width + '%',
-                                    }]}
-                                />
-                            ))}
-                        </View>
-                    )}
+                    <View style={styles.noteContainer}>
+                        <Typography
+                            uppercase
+                        >
+                            {title}
+                        </Typography>
+
+                        {!hasPassword &&
+                            <>
+                                <Typography>
+                                    {note}
+                                </Typography>
+
+                                {images.length > 0 && (
+                                    <View style={styles.noteImages}>
+                                        {images.map((image, index) => (
+                                            <Image
+                                                key={index}
+                                                source={{ uri: image }}
+                                                style={[styles.noteImage, {
+                                                    width: width + '%',
+                                                    height: width + '%',
+                                                }]}
+                                            />
+                                        ))}
+                                    </View>
+                                )}
+                            </>
+                        }
+
+                        {hasPassword &&
+                            <>
+                                <View style={styles.skeletonContainer}>
+                                    <View style={styles.skeleton1} />
+                                    <View style={styles.skeleton2} />
+                                </View>
+
+                                <View style={styles.iconContainer}>
+                                    <Lock
+                                        width={12}
+                                        height={12}
+                                        color={colors.text}
+                                    />
+                                </View>
+                            </>
+                        }
+                    </View>
+
                 </Pressable>
             </Swipeable>
         </Animated.View>
@@ -76,12 +103,15 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingHorizontal: 24,
     },
-    noteContainer: {
+    pressableContainer: {
         minWidth: '100%',
         padding: 20,
-        gap: 8,
         borderRadius: 16,
         backgroundColor: colors.foreground,
+    },
+    noteContainer: {
+        width: '100%',
+        gap: 8,
     },
     noteImages: {
         width: '100%',
@@ -93,5 +123,28 @@ const styles = StyleSheet.create({
     noteImage: {
         aspectRatio: 1,
         borderRadius: 16,
+    },
+    skeletonContainer: {
+        width: '100%',
+        gap: 8,
+    },
+    skeleton1: {
+        width: '80%',
+        height: 10,
+        borderRadius: 6,
+        backgroundColor: colors.text5,
+    },
+    skeleton2: {
+        width: '60%',
+        height: 10,
+        borderRadius: 16,
+        backgroundColor: colors.text5,
+    },
+    iconContainer: {
+        position: 'absolute',
+        right: 0,
+        padding: 8,
+        borderRadius: 16,
+        backgroundColor: colors.text5,
     },
 })
