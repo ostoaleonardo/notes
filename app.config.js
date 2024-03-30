19545b270bf07fac1738f7a8287605d1ede0d5c8
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-03-30 10:53:44 -0600

    Add app variants

---
+++ app.config.js
@@ -0,0 +1,44 @@
+const DEV = process.env.VARIANT === 'development'
+
+const NAME = DEV ? 'Notes (Dev)' : 'Notes'
+const PACKAGE = DEV ? 'com.monospace.notes.dev' : 'com.monospace.notes'
+
+export default {
+  name: NAME,
+  slug: 'notes',
+  scheme: 'notes',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'dark',
+  backgroundColor: '#09090b',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#09090b'
+  },
+  assetBundlePatterns: [
+    '**/*'
+  ],
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#09090b'
+    },
+    package: PACKAGE,
+    versionCode: 2
+  },
+  plugins: [
+    'expo-router',
+    'expo-font',
+    'expo-localization'
+  ],
+  extra: {
+    router: {
+      origin: false
+    },
+    eas: {
+      projectId: '319d937a-0136-4d14-b677-d958cc1b7996'
+    }
+  }
+}
