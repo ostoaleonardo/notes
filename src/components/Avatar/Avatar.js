4227d2c36add4cac23ba409bef83cc0edf6e2eef
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-04-27 11:37:38 -0600

    Add Avatar and User components to display user information

---
+++ src/components/Avatar/Avatar.js
@@ -0,0 +1,52 @@
+import { Image, StyleSheet, View } from 'react-native'
+import { useTranslation } from 'react-i18next'
+import { Typography } from '../Text'
+import { useAuth } from '@/hooks'
+import { COLORS } from '@/constants'
+
+const READ_ONLY = true
+
+export function Avatar({ color }) {
+    const { t } = useTranslation()
+    const { user } = useAuth(READ_ONLY)
+    const { givenName, photo } = user
+
+    const containerStyles = {
+        ...styles.container,
+        backgroundColor: color || COLORS.foreground
+    }
+
+    if (!givenName) return (
+        <Typography
+            opacity={0.5}
+            variant='caption'
+        >
+            ({t('settings.signIn')})
+        </Typography>
+    )
+
+    return (
+        photo ? (
+            <Image
+                source={{ uri: photo }}
+                style={containerStyles}
+            />
+        ) : (
+            <View style={containerStyles}>
+                <Typography>
+                    {givenName[0]}
+                </Typography>
+            </View>
+        )
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: 32,
+        height: 32,
+        borderRadius: 16,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
