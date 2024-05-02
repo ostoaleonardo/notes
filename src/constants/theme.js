aed252dcac15ff24079ac8ea1e2aa2c77a19d55d
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-05-01 21:01:29 -0600

    Update theme constants

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,11 +1,18 @@
+const FOREGROUND = '#18181b'
 const PRIMARY = '#c8102e'
 const TEXT = '#ffffff'
 
 export const COLORS = {
     background: '#09090b',
-    foreground: '#18181b',
     overlay: '#00000080',
     transparent: 'transparent',
+
+    foreground: FOREGROUND,
+    foreground75: `${FOREGROUND}bf`,
+    foreground50: `${FOREGROUND}80`,
+    foreground25: `${FOREGROUND}40`,
+    foreground15: `${FOREGROUND}26`,
+    foreground5: `${FOREGROUND}0d`,
 
     primary: PRIMARY,
     primary15: `${PRIMARY}26`,
