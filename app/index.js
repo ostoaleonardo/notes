20ea3fc4cc04d5f2f1fa91a545df14504d7599c8
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-05-04 21:31:16 -0600

    Update app component to handle authentication and redirection

--- app/index.js
+++ app/index.js
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react'
 import { Redirect } from 'expo-router'
+import { useAuth } from '@/hooks'
 import { ROUTES } from '@/constants'
 
 export default function App() {
+    const [isReady, setIsReady] = useState(false)
+    const { isSignedIn, setIsSignedIn, getIsSignedIn } = useAuth()
+
+    useEffect(() => {
+        (async () => {
+            const isSignedIn = await getIsSignedIn()
+            setIsSignedIn(isSignedIn)
+            setIsReady(true)
+        })()
+    }, [])
+
+    if (!isReady) return null
+
+    if (!isSignedIn) {
+        return <Redirect href={ROUTES.SIGN_IN} />
+    }
+
     return <Redirect href={ROUTES.HOME} />
 }
