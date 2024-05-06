ecd1e3ad50dd6273dd92828e72aea1e1ea6de013
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-05-05 21:22:48 -0600

    Update hooks index file

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -5,7 +5,9 @@ export { useGoogleDrive } from './useGoogleDrive'
 // Notes
 export { useNotes } from './useNotes'
 export { useCategories } from './useCategories'
+export { useStorage } from './useStorage'
 
 // UI
 export { useLanguage } from './useLanguage'
 export { useHeaderTitle } from './useHeaderTitle'
+
