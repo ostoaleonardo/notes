0d8e16776ee0ab481a2f68f49624a546011e59c5
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-05-05 19:58:43 -0600

    Add constants for local storage management

---
+++ src/constants/storageKeys.js
@@ -0,0 +1,7 @@
+export const STORAGE_KEYS = {
+    NOTES: 'notes',
+    CATEGORIES: 'categories',
+    LAST_SYNC: 'lastSync',
+    NOTES_FILE_ID: 'notesFileId',
+    CATEGORIES_FILE_ID: 'categoriesFileId'
+}
