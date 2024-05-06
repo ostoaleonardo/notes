f8f28078c3b8e431d483545ba8eee74efa1a143c
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-05-05 20:43:20 -0600

    Update useNotes hook to include sync

--- src/hooks/useNotes.js
+++ src/hooks/useNotes.js
@@ -1,31 +1,24 @@
 import { useContext, useEffect, useState } from 'react'
+import { useGoogleDrive } from './useGoogleDrive'
+import { useStorage } from './useStorage'
 import { NoteContext } from '@/context'
-import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export function useNotes() {
     const { notes, setNotes } = useContext(NoteContext)
+    const { isSyncing, uploadBackup } = useGoogleDrive()
+    const { setItem, getItem } = useStorage()
     const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        AsyncStorage.getItem('notes')
-            .then((data) => {
-                if (data) {
-                    setNotes(JSON.parse(data))
-                }
-                setLoading(false)
-            })
-    }, [])
 
     const saveNote = (note) => {
         const newNotes = [note, ...notes]
         setNotes(newNotes)
-        AsyncStorage.setItem('notes', JSON.stringify(newNotes))
+        updateBackup(newNotes)
     }
 
     const deleteNote = (id) => {
         const newNotes = notes.filter((note) => note.id !== id)
         setNotes(newNotes)
-        AsyncStorage.setItem('notes', JSON.stringify(newNotes))
+        updateBackup(newNotes)
     }
 
     const updateNote = (note) => {
@@ -35,16 +28,37 @@ export function useNotes() {
             }
             return n
         })
+
         setNotes(newNotes)
-        AsyncStorage.setItem('notes', JSON.stringify(newNotes))
+        updateBackup(newNotes)
     }
 
     const getNote = (id) => {
         return notes.find((note) => note.id === id)
     }
 
+    const updateBackup = async (newNotes) => {
+        await setItem('notes', JSON.stringify(newNotes))
+        await uploadBackup('notesFileId', 'notes.json', JSON.stringify(newNotes))
+    }
+
+    useEffect(() => {
+        (async () => {
+            if (isSyncing) return
+
+            const notes = await getItem('notes')
+
+            if (notes) {
+                setNotes(JSON.parse(notes))
+            }
+
+            setLoading(false)
+        })()
+    }, [])
+
     return {
         notes,
+        setNotes,
         loading,
         saveNote,
         deleteNote,
