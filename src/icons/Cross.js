14f00bd7c64ce5d7420442d7814b670b92759968
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-05-07 20:59:35 -0600

    Update icons

--- src/icons/Cross.js
+++ src/icons/Cross.js
@@ -1,7 +1,7 @@
 import { Path, Svg } from 'react-native-svg'
 
 export const Cross = (props) => (
-    <Svg viewBox='0 0 24 24' fill='currentColor' {...props}>
-        <Path d='M11.5 12.5H6v-1h5.5V6h1v5.5H18v1h-5.5V18h-1v-5.5Z' />
+    <Svg viewBox='0 -960 960 960' fill='currentColor' {...props}>
+        <Path d='M460-460H240v-40h220v-220h40v220h220v40H500v220h-40v-220Z' />
     </Svg>
 )
