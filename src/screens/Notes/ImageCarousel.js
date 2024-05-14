ba4931d6891a32029d23f544e01166414b05c097
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-05-13 21:03:43 -0600

    Update ImageCarousel component to conditionally render remove button

--- src/screens/Notes/ImageCarousel.js
+++ src/screens/Notes/ImageCarousel.js
@@ -3,51 +3,47 @@ import Animated, { LinearTransition } from 'react-native-reanimated'
 import { ImagePreview, PickerImage, Scroll } from '@/components'
 import { COLORS } from '@/constants'
 
-export function ImageCarousel({ images, onAddImage, onOpenImage, onRemoveImage }) {
+export function ImageCarousel({ images, onAddImage, onOpenImage, onRemoveImage, readOnly }) {
     return (
         <Scroll
             horizontal
             contentStyle={styles.container}
+            containerStyle={{ maxHeight: 100 }}
         >
             {images.length > 0 &&
-                <View style={styles.imagesContainer}>
-                    {images.map((image, index) => (
-                        <ImagePreview
-                            key={index}
-                            image={image}
-                            openImage={() => onOpenImage(index)}
-                            removeImage={() => onRemoveImage(image)}
-                        />
-                    ))}
-                </View>
+                images.map((image, index) => (
+                    <ImagePreview
+                        key={index}
+                        image={image}
+                        openImage={() => onOpenImage(index)}
+                        removeImage={!readOnly && (() => onRemoveImage(index))}
+                    />
+                ))
             }
-            <Animated.View
-                layout={LinearTransition}
-                style={styles.pickerContainer}
-            >
-                <PickerImage
-                    pickCamera
-                    setImage={onAddImage}
-                />
-                <View style={styles.separator} />
-                <PickerImage setImage={onAddImage} />
-            </Animated.View>
+
+            {!readOnly && (
+                <Animated.View
+                    layout={LinearTransition}
+                    style={styles.pickerContainer}
+                >
+                    <PickerImage
+                        pickCamera
+                        setImage={onAddImage}
+                    />
+                    <View style={styles.separator} />
+                    <PickerImage setImage={onAddImage} />
+                </Animated.View>
+            )}
         </Scroll>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         width: '100%',
         gap: 8,
-        marginTop: 16,
         flexDirection: 'row',
         paddingHorizontal: 24,
-    },
-    imagesContainer: {
-        gap: 8,
-        flexDirection: 'row',
     },
     pickerContainer: {
         borderRadius: 24,
