ca1c5424d9491b541c142480648d576acd64ef88
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-05-20 20:26:58 -0600

    Update cross icon

--- src/icons/Cross.js
+++ src/icons/Cross.js
@@ -2,6 +2,6 @@ import { Path, Svg } from 'react-native-svg'
 
 export const Cross = (props) => (
     <Svg viewBox='0 -960 960 960' fill='currentColor' {...props}>
-        <Path d='M460-460H240v-40h220v-220h40v220h220v40H500v220h-40v-220Z' />
+        <Path d='M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z' />
     </Svg>
 )
