f596de77e6a40404086bc6e51fdaa0c84cb61cfd
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-05-23 20:29:57 -0600

    Update scroll component to remove view wrapper

--- src/components/Scroll/Scroll.js
+++ src/components/Scroll/Scroll.js
@@ -1,17 +1,14 @@
-import { ScrollView, View } from 'react-native'
+import { ScrollView } from 'react-native'
 
 export function Scroll({ children, containerStyle, contentStyle, ...props }) {
     return (
         <ScrollView
             {...props}
-            bounces={true}
             style={containerStyle}
             showsVerticalScrollIndicator={false}
             showsHorizontalScrollIndicator={false}
         >
-            <View style={contentStyle}>
-                {children}
-            </View>
+            {children}
         </ScrollView>
     )
 }
