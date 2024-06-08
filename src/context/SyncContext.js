93f7bfb6c25a666fe1c1a9d9da8889463c39586d
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-06-08 13:50:02 -0600

    Delete state from sync context

--- src/context/SyncContext.js
+++ src/context/SyncContext.js
@@ -3,14 +3,11 @@ import { createContext, useState } from 'react'
 export const SyncContext = createContext()
 
 export function SyncProvider({ children }) {
-    const [lastSync, setLastSync] = useState('')
     const [isSyncing, setIsSyncing] = useState(false)
 
     return (
         <SyncContext.Provider
             value={{
-                lastSync,
-                setLastSync,
                 isSyncing,
                 setIsSyncing
             }}
