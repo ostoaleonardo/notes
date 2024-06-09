5252c5441553c40cc32660d153613b7d74635cb9
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-06-09 13:22:51 -0600

    Remove unused storage key for last sync

--- src/constants/storageKeys.js
+++ src/constants/storageKeys.js
@@ -1,7 +1,6 @@
 export const STORAGE_KEYS = {
     NOTES: 'notes',
     CATEGORIES: 'categories',
-    LAST_SYNC: 'lastSync',
     CATEGORIES_FILE_ID: 'categoriesFileId',
     NOTES_ID_BACKUP: 'notesIdBackup',
     NOTES_TO_SYNC: 'notesToSync'
