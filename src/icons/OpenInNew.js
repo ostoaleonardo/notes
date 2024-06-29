a1c61b3af66cccb43981bc98e2922f1c7bb450aa
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-06-29 11:44:18 -0600

    Add OpenInNew icon

---
+++ src/icons/OpenInNew.js
@@ -0,0 +1,7 @@
+import { Path, Svg } from 'react-native-svg'
+
+export const OpenInNew = (props) => (
+    <Svg width='24px' height='24px' viewBox='0 -960 960 960' fill='currentColor' {...props}>
+        <Path d='M172-172v-616h268v28H200v560h560v-240h28v268H172Zm218-198-20-20 370-370H560v-28h228v228h-28v-180L390-370Z' />
+    </Svg>
+)
