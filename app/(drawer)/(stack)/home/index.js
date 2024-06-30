21de9520d2de5722ed1aeb3b9ca8692877f53c62
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-06-30 12:52:34 -0600

    Update home screen

--- app/(drawer)/(stack)/home/index.js
+++ app/(drawer)/(stack)/home/index.js
@@ -1,31 +1,40 @@
 import { useState } from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, View } from 'react-native'
+import { View } from 'react-native'
 import { useTranslation } from 'react-i18next'
 import { FloatingButton } from '@/components'
-import { NotesContainer, FilterCarousel } from '@/screens'
-import { useHeaderTitle } from '@/hooks'
-import { COLORS } from '@/constants'
+import { NotesContainer, FilterCarousel, DeleteNote } from '@/screens'
+import { useBottomSheet, useHeaderTitle } from '@/hooks'
 
 export default function App() {
     const { t } = useTranslation()
+    const { ref, onOpen, onClose } = useBottomSheet()
+    const [selectedNote, setSelectedNote] = useState(null)
     const [filteredNotes, setFilteredNotes] = useState([])
 
     useHeaderTitle(t('header.notes'))
 
     return (
-        <View style={styles.container}>
+        <View style={{ flex: 1 }}>
             <FilterCarousel setFilteredNotes={setFilteredNotes} />
-            <NotesContainer filteredNotes={filteredNotes} />
-            <FloatingButton label={t('button.addNote')} href='/note' />
-            <StatusBar style='auto' />
+            <NotesContainer
+                onOpen={onOpen}
+                filteredNotes={filteredNotes}
+                selectedNote={selectedNote}
+                setSelectedNote={setSelectedNote}
+            />
+
+            <FloatingButton
+                label={t('button.addNote')}
+                href='/note'
+            />
+            <DeleteNote
+                ref={ref}
+                id={selectedNote}
+                onClose={() => {
+                    setSelectedNote(null)
+                    onClose()
+                }}
+            />
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: COLORS.background,
-    },
-})
