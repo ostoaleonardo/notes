61b04f7a7a7208a49c126cf46d48d3879f0846ec
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-07-03 14:42:32 -0600

    feat: Add new routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,10 @@
 export const ROUTES = {
     SIGN_IN: '/signin',
-    HOME: '/home'
+    HOME: '/home',
+    CATEGORIES: '/categories',
+    ADD_NOTE: '(notes)',
+    VIEW_NOTE: '/view/',
+    UNLOCK_NOTE: '/unlock/',
+    EDIT_NOTE: '/edit/',
+    SETTINGS: '/settings'
 }
