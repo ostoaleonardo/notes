907779e4b4f773a3bc1a84d85827f3cf40a758c9
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-07-11 14:46:12 -0600

    Remove unused hooks

--- src/hooks/useUser.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import { AuthContext } from '@/context'
-
-export function useUser() {
-    const { user, setUser } = useContext(AuthContext)
-    
-    return { user, setUser }
-}
