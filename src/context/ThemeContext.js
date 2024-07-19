f7f236424c156786af9330018aa0c09771cdcdee
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-07-19 10:56:16 -0600

    🛠️ Update theme context

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,30 +1,36 @@
 import { createContext, useEffect, useState } from 'react'
 import { useColorScheme } from 'react-native'
+import { PaperProvider } from 'react-native-paper'
 import { THEMES } from '@/constants'
 
 export const ThemeContext = createContext()
 
-export function ThemeProvider({ initialTheme, setTheme, children }) {
-    const [userTheme, setUserTheme] = useState('system')
+export function ThemeProvider({ initialTheme, children }) {
+    const [mode, setMode] = useState(initialTheme.mode)
+    const [name, setName] = useState(initialTheme.name)
+    const [theme, setTheme] = useState(initialTheme.theme)
     const colorScheme = useColorScheme()
 
     useEffect(() => {
-        const theme = userTheme !== 'system' ? userTheme : colorScheme
+        const theme = mode !== 'system' ? mode : colorScheme
         setTheme(THEMES[theme])
-    }, [userTheme, colorScheme])
-
-    useEffect(() => {
-        setUserTheme(initialTheme.mode)
-    }, [initialTheme])
+        setName(theme)
+    }, [mode, colorScheme])
 
     return (
         <ThemeContext.Provider
             value={{
-                userTheme,
-                setUserTheme
+                mode,
+                setMode,
+                name,
+                setName,
+                theme,
+                setTheme
             }}
         >
-            {children}
+            <PaperProvider theme={theme}>
+                {children}
+            </PaperProvider>
         </ThemeContext.Provider>
     )
 }
