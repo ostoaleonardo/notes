0490ed040f52fb7216eefc942172e786755e0c35
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-07-19 10:59:23 -0600

    🛠️ Refactor providers component

--- app/providers.js
+++ app/providers.js
@@ -1,33 +1,17 @@
-import { useEffect, useState } from 'react'
-import { PaperProvider } from 'react-native-paper'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
-import { AuthProvider, NoteProvider, ThemeProvider } from '@/context'
-import { THEMES } from '@/constants'
+import { AuthProvider, NoteProvider } from '@/context'
 
-export function Providers({ initialTheme, children }) {
-    const [theme, setTheme] = useState({})
-
-    useEffect(() => {
-        setTheme(THEMES[initialTheme.name])
-    }, [initialTheme])
-
+export function Providers({ children }) {
     return (
-        <ThemeProvider
-            initialTheme={initialTheme}
-            setTheme={setTheme}
-        >
-            <PaperProvider theme={theme}>
-                <GestureHandlerRootView style={{ flex: 1 }}>
-                    <AuthProvider>
-                        <NoteProvider>
-                            <BottomSheetModalProvider>
-                                {children}
-                            </BottomSheetModalProvider>
-                        </NoteProvider>
-                    </AuthProvider>
-                </GestureHandlerRootView>
-            </PaperProvider>
-        </ThemeProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <AuthProvider>
+                <NoteProvider>
+                    <BottomSheetModalProvider>
+                        {children}
+                    </BottomSheetModalProvider>
+                </NoteProvider>
+            </AuthProvider>
+        </GestureHandlerRootView>
     )
 }
