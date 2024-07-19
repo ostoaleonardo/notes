2c8f75227e838cb9693f2e5559b71d9f60e52072
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-07-19 10:58:24 -0600

    🛠️ Add status bar to home screen

--- app/(app)/(stack)/(drawer)/home/index.js
+++ app/(app)/(stack)/(drawer)/home/index.js
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 import { useTranslation } from 'react-i18next'
 import { FloatingButton } from '@/components'
 import { NotesContainer, FilterCarousel, DeleteNote } from '@/screens'
 import { useBottomSheet } from '@/hooks'
+import { ThemeContext } from '@/context'
 import { ROUTES } from '@/constants'
 
 export default function App() {
     const { t } = useTranslation()
+    const { name } = useContext(ThemeContext)
     const { ref, onOpen, onClose } = useBottomSheet()
     const [selectedNote, setSelectedNote] = useState(null)
     const [filteredNotes, setFilteredNotes] = useState([])
@@ -34,6 +37,8 @@ export default function App() {
                     onClose()
                 }}
             />
+
+            <StatusBar style={name === 'light' ? 'dark' : 'light'} />
         </View>
     )
 }
