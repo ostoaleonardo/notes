8b1cf35d94e5965496d2111fb698db5e38f90f9d
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-07-20 20:40:57 -0600

    🛠️ Update initial theme state in theme provider

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -6,10 +6,16 @@ import { THEMES } from '@/constants'
 export const ThemeContext = createContext()
 
 export function ThemeProvider({ initialTheme, children }) {
-    const [mode, setMode] = useState(initialTheme.mode)
-    const [name, setName] = useState(initialTheme.name)
-    const [theme, setTheme] = useState(initialTheme.theme)
+    const [mode, setMode] = useState('')
+    const [name, setName] = useState('')
+    const [theme, setTheme] = useState({})
     const colorScheme = useColorScheme()
+
+    useEffect(() => {
+        setMode(initialTheme.mode)
+        setName(initialTheme.name)
+        setTheme(initialTheme.theme)
+    }, [initialTheme])
 
     useEffect(() => {
         const theme = mode !== 'system' ? mode : colorScheme
