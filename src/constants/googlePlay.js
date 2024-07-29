37a28e262fcb24fe2f6a4693af89626b46b86b64
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-07-29 13:58:22 -0600

    📄 Add links constants

--- src/constants/googlePlay.js
+++
@@ -1,1 +0,0 @@
-export const GOOGLE_PLAY_URL = 'https://play.google.com/store/apps/details?id=com.monospace.notes'
---
+++ src/constants/links.js
@@ -0,0 +1,5 @@
+export const LINKS = {
+    GOOGLE_PLAY: 'https://play.google.com/store/apps/details?id=com.monospace.notes',
+    GITHUB: 'https://github.com/ostoaleonardo/notes',
+    TRANSLATIONS: 'https://crowdin.com/project/monospace-notes'
+}
