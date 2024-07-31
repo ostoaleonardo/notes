48a5abc31ac51e1495ab3a3b9c343481da1c83e0
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-07-31 15:33:00 -0600

    ✨ Add item list icons

---
+++ src/icons/CheckList.js
@@ -0,0 +1,7 @@
+import { Path, Svg } from 'react-native-svg'
+
+export const CheckList = (props) => (
+    <Svg width='24' height='24' viewBox='0 -960 960 960' fill='currentColor' {...props}>
+        <Path d='M233.54-243.85 120-357.38l27.54-27.54 85 85 170-170 27.54 28.54-196.54 197.53Zm0-289.23L120-646.62l27.54-27.53 85 85 170-170 27.54 28.53-196.54 197.54Zm287.23 217.7v-40h320v40h-320Zm0-289.24v-40h320v40h-320Z' />
+    </Svg>
+)
---
+++ src/icons/NumberedList.js
@@ -0,0 +1,7 @@
+import { Path, Svg } from 'react-native-svg'
+
+export const NumberedList = (props) => (
+    <Svg width='24' height='24' viewBox='0 -960 960 960' fill='currentColor' {...props}>
+        <Path d='M160-120v-35.38h100V-210h-60v-35.38h60V-300H160v-35.38h135.38v90l-27.69 20.76L295.38-210v90H160Zm0-252.31v-125.38h100v-54.62H160v-35.38h135.38v125.38h-100v54.62h100v35.38H160Zm60-252.31v-180h-60V-840h95.38v215.38H220ZM384.62-220v-40H800v40H384.62Zm0-240v-40H800v40H384.62Zm0-240v-40H800v40H384.62Z' />
+    </Svg>
+)
