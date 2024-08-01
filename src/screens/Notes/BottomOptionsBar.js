e68121fa232834cc928fdfc9afeb7f5e3de6475c
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-07-31 18:48:55 -0600

    🛠️ Add menu to display list types

--- src/screens/Notes/BottomOptionsBar.js
+++ src/screens/Notes/BottomOptionsBar.js
@@ -1,13 +1,27 @@
+import { useState } from 'react'
 import { View } from 'react-native'
-import { useTheme } from 'react-native-paper'
+import { useTranslation } from 'react-i18next'
+import { Menu, useTheme } from 'react-native-paper'
 import { IconButton } from '@/components'
 import { openImagePicker } from '@/utils'
-import { Camera, Check, CheckBox, Lock, Picture, Unlock } from '@/icons'
+import { BulletedList, Camera, Check, CheckList, Lock, NumberedList, Picture, Unlock } from '@/icons'
+import { FONTS } from '@/constants'
 
-export function BottomOptionsBar({ onAddImage, hasPassword, onAddItemList, onOpenPassword, onSave }) {
+const menuItemStyle = {
+    fontSize: 12,
+    textTransform: 'uppercase',
+    fontFamily: FONTS.azeretLight
+}
+
+export function BottomOptionsBar({ onAddImage, onListType, hasPassword, onOpenPassword, onSave }) {
+    const { t } = useTranslation()
     const { colors } = useTheme()
+    const [menuVisible, setMenuVisible] = useState(false)
 
     const iconProps = { color: colors.onBackground }
+
+    const onOpenMenu = () => setMenuVisible(true)
+    const onCloseMenu = () => setMenuVisible(false)
 
     const handleImagePicker = async (type) => {
         const assets = await openImagePicker(type)
@@ -41,11 +55,43 @@ export function BottomOptionsBar({ onAddImage, hasPassword, onAddItemList, onOpe
                     onPress={() => handleImagePicker('gallery')}
                     icon={<Picture {...iconProps} />}
                 />
-                <IconButton
-                    variant='light'
-                    onPress={onAddItemList}
-                    icon={<CheckBox {...iconProps} />}
-                />
+
+                <Menu
+                    elevation={0}
+                    visible={menuVisible}
+                    onDismiss={onCloseMenu}
+                    anchor={
+                        <IconButton
+                            variant='light'
+                            onPress={onOpenMenu}
+                            icon={<CheckList {...iconProps} />}
+                        />
+                    }
+                    contentStyle={{
+                        borderRadius: 24,
+                        overflow: 'hidden',
+                        backgroundColor: colors.surface
+                    }}
+                >
+                    <Menu.Item
+                        title={t('list.bulleted')}
+                        titleStyle={menuItemStyle}
+                        onPress={() => onListType('bulleted')}
+                        leadingIcon={() => <BulletedList {...iconProps} />}
+                    />
+                    <Menu.Item
+                        title={t('list.numbered')}
+                        titleStyle={menuItemStyle}
+                        onPress={() => onListType('numbered')}
+                        leadingIcon={() => <NumberedList {...iconProps} />}
+                    />
+                    <Menu.Item
+                        title={t('list.checklist')}
+                        titleStyle={menuItemStyle}
+                        onPress={() => onListType('checklist')}
+                        leadingIcon={() => <CheckList {...iconProps} />}
+                    />
+                </Menu>
             </View>
 
             <View style={{ flexDirection: 'row', gap: 8 }}>
