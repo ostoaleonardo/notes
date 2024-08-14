faee7d6db2632c91ab29b5ec6f233cda0924940e
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-08-13 22:03:00 -0600

    📦 Add ReactNativePackager and Widget module

--- android/app/src/main/java/com/monospace/notes/storage/ReactNativePackager.kt
+++ android/app/src/main/java/com/monospace/notes/storage/ReactNativePackager.kt
@@ -5,7 +5,7 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 
-class SharedStoragePackager : ReactPackage {
+class ReactNativePackager : ReactPackage {
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
         return emptyList()
     }
@@ -13,6 +13,7 @@ class SharedStoragePackager : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
         val modules: MutableList<NativeModule> = ArrayList()
         modules.add(SharedStorage(reactContext))
+        modules.add(Widget(reactContext))
         return modules
     }
 }
