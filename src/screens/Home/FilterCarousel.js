6b5c7623e42189739b3180a8b0e14c2d8d00241f
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-10-09 21:25:20 -0600

    🛠️ Refactor filter carousel component

--- src/screens/Home/FilterCarousel.js
+++ src/screens/Home/FilterCarousel.js
@@ -1,11 +1,13 @@
 import { StyleSheet, View } from 'react-native'
-import { useTranslation } from 'react-i18next'
-import { Chip, Scroll } from '@/components'
+import { IconButton, useTheme } from 'react-native-paper'
+import { AnimatedView, Chip, Scroll } from '@/components'
 import { useCategories } from '@/hooks'
+import { Close } from '@/icons'
 
-export function FilterCarousel({ selectedFilter, setSelectedFilter }) {
-    const { t } = useTranslation()
+export function FilterCarousel({ filter, onFilter }) {
+    const { colors } = useTheme()
     const { categories } = useCategories()
+    const { onBackground } = colors
 
     return (
         <View style={styles.container}>
@@ -14,14 +16,29 @@ export function FilterCarousel({ selectedFilter, setSelectedFilter }) {
                 overScrollMode='never'
                 contentContainerStyle={styles.scroll}
             >
-                {categories.map(({ id, name }) => (
-                    <Chip
-                        key={id}
-                        onPress={() => setSelectedFilter(id)}
-                        variant={id === selectedFilter ? 'primary' : 'bordered'}
-                        label={id === 'all' ? t('categories.all') : name}
-                    />
-                ))}
+                {filter.size > 0 && (
+                    <AnimatedView>
+                        <IconButton
+                            size={12}
+                            mode='outlined'
+                            onPress={() => onFilter('all')}
+                            icon={() => <Close color={onBackground} />}
+                        />
+                    </AnimatedView>
+                )}
+
+                {categories
+                    .filter(({ id }) => id !== 'all')
+                    .map(({ id, name }) => (
+                        <Chip
+                            key={id}
+                            onPress={() => onFilter(id)}
+                            mode={filter.has(id) ? 'flat' : 'outlined'}
+                        >
+                            {name}
+                        </Chip>
+                    ))
+                }
             </Scroll>
         </View>
     )
@@ -29,12 +46,14 @@ export function FilterCarousel({ selectedFilter, setSelectedFilter }) {
 
 const styles = StyleSheet.create({
     container: {
-        width: '100%'
+        width: '100%',
+        height: 48
     },
     scroll: {
         flexGrow: 1,
-        gap: 8,
-        paddingVertical: 16,
+        gap: 4,
+        marginBottom: 8,
+        alignItems: 'center',
         paddingHorizontal: 24
     }
 })
