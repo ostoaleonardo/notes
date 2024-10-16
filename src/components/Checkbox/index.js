908376b32674dcd8f0f4e7ff0782991ba03cf5fe
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-10-15 21:26:24 -0600

    🛠️ Add checkbox component

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -1,13 +1,13 @@
-import { Check } from '@/icons'
 import { StyleSheet, View } from 'react-native'
 import { useTheme } from 'react-native-paper'
+import { Check } from '@/icons'
 
 export function Checkbox({ checked }) {
     const { colors } = useTheme()
     const { background, onBackground } = colors
 
     const backgroundColor = checked ? onBackground : 'transparent'
-    const borderColor = checked ? onBackground : onBackground + '1a'
+    const borderColor = checked ? onBackground : onBackground + '33'
 
     return (
         <View
