d7d5039dd2699070083c305872ab133317186f0a
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-10-26 18:25:53 -0600

    🛠️ Update providers

--- app/providers.js
+++ app/providers.js
@@ -1,16 +1,18 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
-import { AuthProvider, NoteProvider } from '@/context'
+import { AuthProvider, NoteProvider, UtilsProvider } from '@/context'
 
 export function Providers({ children }) {
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
             <AuthProvider>
-                <NoteProvider>
-                    <BottomSheetModalProvider>
-                        {children}
-                    </BottomSheetModalProvider>
-                </NoteProvider>
+                <UtilsProvider>
+                    <NoteProvider>
+                        <BottomSheetModalProvider>
+                            {children}
+                        </BottomSheetModalProvider>
+                    </NoteProvider>
+                </UtilsProvider>
             </AuthProvider>
         </GestureHandlerRootView>
     )
