9b02c64e4bc9758bed89e8443ffaab1525c43337
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-11-03 16:53:37 -0600

    ✨ Update icons

---
+++ src/icons/KeepFilled.js
@@ -0,0 +1,7 @@
+import { Path, Svg } from 'react-native-svg'
+
+export const KeepFilled = (props) => (
+    <Svg width='24' height='24' viewBox='0 -960 960 960' fill='currentColor' {...props}>
+        <Path d='M600-463.08 663.08-400v40H500v200l-20 20-20-20v-200H296.92v-40L360-463.08V-760h-40v-40h320v40h-40v296.92Z' />
+    </Svg>
+)
---
+++ src/icons/MarkdownFilled.js
@@ -0,0 +1,7 @@
+import { Path, Svg } from 'react-native-svg'
+
+export const MarkdownFilled = (props) => (
+    <Svg width='24' height='24' viewBox='0 -960 960 960' fill='currentColor' {...props}>
+        <Path d='m640-372.31 102.31-102.31-24.31-25.3-60.31 59.54v-147.31h-35.38v147.31L562-499.92l-24.31 25.3L640-372.31ZM120-200v-560h720v560H120Zm112.31-172.31h35.38v-180h64.62v120h35.38v-120h64.62v180h35.38v-215.38H232.31v215.38Z' />
+    </Svg>
+)
