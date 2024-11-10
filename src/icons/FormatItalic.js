8908a184c5103399415c0d7ed7413a3497b4e27f
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-11-09 19:24:09 -0600

    ✨ Add icons

---
+++ src/icons/FormatItalic.js
@@ -0,0 +1,7 @@
+import { Path, Svg } from 'react-native-svg'
+
+export const FormatItalic = (props) => (
+    <Svg width='24' height='24' viewBox='0 -960 960 960' fill='currentColor' {...props}>
+        <Path d='M231.54-230v-44.62h145.38l139.23-410.76H370.77V-730h332.31v44.62H562.31L423.08-274.62h140.77V-230H231.54Z' />
+    </Svg>
+)
---
+++ src/icons/FormatStrikethrough.js
@@ -0,0 +1,7 @@
+import { Path, Svg } from 'react-native-svg'
+
+export const FormatStrikethrough = (props) => (
+    <Svg width='24' height='24' viewBox='0 -960 960 960' fill='currentColor' {...props}>
+        <Path d='M120-420.77v-40h720v40H120Zm335.38-118.46V-710H240.77v-50H720v50H505.38v170.77h-50Zm0 339.23v-142.31h50V-200h-50Z' />
+    </Svg>
+)
