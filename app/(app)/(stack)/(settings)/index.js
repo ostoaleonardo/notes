3f2e470596ef32be7987e170d6a5fedb1329f952
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2025-03-29 20:03:02 -0600

    🛠️ Refactor settings screen

--- app/(app)/(stack)/(settings)/index.js
+++ app/(app)/(stack)/(settings)/index.js
@@ -3,37 +3,16 @@ import { router } from 'expo-router'
 import { Switch, useTheme } from 'react-native-paper'
 import { useTranslation } from 'react-i18next'
 import { Section, Separator, Scroll } from '@/components'
-import { AppVersionCard, ExportBackup, ImportBackup, Languages, Option } from '@/screens'
-import { useBottomSheet, useFiles, useMarkdown } from '@/hooks'
+import { AppVersionCard, Languages, Option } from '@/screens'
+import { useBottomSheet, useMarkdown } from '@/hooks'
 import { ArrowForward, OpenInNew } from '@/icons'
 import { LINKS, ROUTES } from '@/constants'
-import { useState } from 'react'
 
 export default function Settings() {
     const { t } = useTranslation()
     const { colors } = useTheme()
-    const { importFile } = useFiles()
     const { markdown, toggleMarkdown } = useMarkdown()
     const { ref, onOpen, onClose } = useBottomSheet()
-
-    const [code, setCode] = useState('')
-    const [importVisible, setImportVisible] = useState(false)
-    const [exportVisible, setExportVisible] = useState(false)
-
-    const showImportDialog = () => setImportVisible(true)
-    const hideImportDialog = () => setImportVisible(false)
-
-    const showExportDialog = () => setExportVisible(true)
-    const hideExportDialog = () => setExportVisible(false)
-
-    const importBackup = async () => {
-        const code = await importFile()
-
-        if (code !== 'canceled') {
-            setCode(code)
-            showImportDialog()
-        }
-    }
 
     const onToggleMarkdown = () => toggleMarkdown(!markdown)
 
@@ -75,25 +54,6 @@ export default function Settings() {
 
                 <Separator style={{ marginHorizontal: 24 }} />
 
-                {/* <Section
-                    title={t('title.backup')}
-                >
-                    <Option
-                        title={t('backup.import.title')}
-                        description={t('backup.fromFile')}
-                        rightContent={<ArrowForward {...iconProps} />}
-                        onPress={importBackup}
-                    />
-                    <Option
-                        title={t('backup.export.title')}
-                        description={t('backup.toFile')}
-                        rightContent={<ArrowForward {...iconProps} />}
-                        onPress={showExportDialog}
-                    />
-                </Section>
-
-                <Separator style={{ marginHorizontal: 24 }} /> */}
-
                 <Section
                     title={t('title.about')}
                 >
@@ -117,16 +77,6 @@ export default function Settings() {
                     onClose={onClose}
                 />
             </Scroll>
-
-            <ImportBackup
-                visible={importVisible}
-                onDismiss={hideImportDialog}
-                code={code}
-            />
-            <ExportBackup
-                visible={exportVisible}
-                onDismiss={hideExportDialog}
-            />
         </>
     )
 }
