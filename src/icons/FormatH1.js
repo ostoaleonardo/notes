be9fe7ba969a7efdaf066d6ab007d8f50c630056
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2025-03-29 18:51:08 -0600

    🛠️ Add new format icons

---
+++ src/icons/FormatH1.js
@@ -0,0 +1,7 @@
+import { Path, Svg } from 'react-native-svg'
+
+export const FormatH1 = (props) => (
+    <Svg width='24' height='24' viewBox='0 -960 960 960' fill='currentColor' {...props}>
+        <Path d='M240-300v-360h40v160h200v-160h40v360h-40v-160H280v160h-40Zm440 0v-320h-80v-40h120v360h-40Z' />
+    </Svg>
+)
