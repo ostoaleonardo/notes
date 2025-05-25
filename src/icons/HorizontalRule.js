312139d4a90d18055a72c6b2a36f188a3a756f94
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2025-05-24 19:15:22 -0600

    ✨ Add new icon components

---
+++ src/icons/HorizontalRule.js
@@ -0,0 +1,7 @@
+import { Path, Svg } from 'react-native-svg'
+
+export const HorizontalRule = (props) => (
+    <Svg width='24' height='24' viewBox='0 -960 960 960' fill='currentColor' {...props}>
+        <Path d='M200-460v-40h560v40H200Z' />
+    </Svg>
+)
---
+++ src/icons/Table.js
@@ -0,0 +1,7 @@
+import { Path, Svg } from 'react-native-svg'
+
+export const Table = (props) => (
+    <Svg width='24' height='24' viewBox='0 -960 960 960' fill='currentColor' {...props}>
+        <Path d='M160-160v-640h640v640H160Zm300-223.85H200V-200h260v-183.85Zm40 0V-200h260v-183.85H500Zm-40-40v-185.38H200v185.38h260Zm40 0h260v-185.38H500v185.38ZM200-649.23h560V-760H200v110.77Z' />
+    </Svg>
+)
