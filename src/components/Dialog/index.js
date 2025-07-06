442f6846ae1514d7a974e1745a5a629b8fc9a718
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2025-07-06 17:54:16 -0600

    🛠️ Implement keyboard handling

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -1,12 +1,34 @@
+import { useEffect, useState } from 'react'
+import { Keyboard } from 'react-native'
 import { Dialog, Portal } from 'react-native-paper'
 import { FONTS } from '@/constants'
 
 export function DialogModal({ title, visible, onDismiss, children, actions }) {
+    const [bottom, setBottom] = useState(0)
+
+    useEffect(() => {
+        function onKeyboardChange(e) {
+            if (e?.endCoordinates?.height) {
+                setBottom(e.endCoordinates.height / 2)
+            } else {
+                setBottom(0)
+            }
+        }
+
+        const subscriptions = [
+            Keyboard.addListener('keyboardDidHide', onKeyboardChange),
+            Keyboard.addListener('keyboardDidShow', onKeyboardChange)
+        ]
+
+        return () => subscriptions.forEach((subscription) => subscription.remove())
+    }, [])
+
     return (
         <Portal>
             <Dialog
                 visible={visible}
                 onDismiss={onDismiss}
+                style={{ bottom }}
             >
                 <Dialog.Title
                     style={{
