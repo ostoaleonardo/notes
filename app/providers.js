c7fa53371a58848fabd886a6095d36c3592feb76
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2026-01-01 14:35:26 -0600

    🛠️ Fix hidden content in navigation bar

--- app/providers.js
+++ app/providers.js
@@ -1,10 +1,13 @@
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
 import { AuthProvider, NoteProvider, UtilsProvider } from '@/context'
 
 export default function Providers({ children }) {
+    const { bottom } = useSafeAreaInsets()
+
     return (
-        <GestureHandlerRootView style={{ flex: 1 }}>
+        <GestureHandlerRootView style={{ flex: 1, paddingBottom: bottom }}>
             <AuthProvider>
                 <UtilsProvider>
                     <NoteProvider>
