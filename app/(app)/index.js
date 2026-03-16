b2322a6b088b5e7d8ae6f1737ee945ad654834f7
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2026-03-16 17:42:17 -0600

    ⚒️ Implement silent sign-in

--- app/(app)/index.js
+++ app/(app)/index.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react'
 import { Redirect } from 'expo-router'
 import { useLocalAuthentication } from '@/hooks'
+import { useAuth } from '@/hooks/use-auth'
 import { ROUTES } from '@/constants'
 
 export default function App() {
     useLocalAuthentication()
+    const { signInSilently } = useAuth()
+
+    useEffect(() => {
+        const signIn = async () => {
+            await signInSilently()
+        }
+
+        signIn()
+    }, [])
 
     return <Redirect href={ROUTES.HOME} />
 }
---
+++ src/hooks/use-auth.js
@@ -0,0 +1,127 @@
+import { t } from 'i18next'
+import { useContext, useEffect } from 'react'
+import { ToastAndroid } from 'react-native'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
+import { AuthContext } from '@/context'
+
+export function useAuth() {
+    const {
+        user,
+        setUser,
+        setAccessToken,
+        isSignedIn,
+        setIsSignedIn,
+        setIsAuthenticating
+    } = useContext(AuthContext)
+
+    const signIn = async () => {
+        setIsAuthenticating(true)
+
+        try {
+            await GoogleSignin.hasPlayServices()
+            const { data, type } = await GoogleSignin.signIn()
+            const { user } = data
+
+            if (user) {
+                const accessToken = await getAccessToken()
+                setUser(user)
+                setAccessToken(accessToken)
+                setIsSignedIn(true)
+            }
+
+            return null
+        } catch (error) {
+            switch (error.code) {
+                case statusCodes.IN_PROGRESS:
+                    break
+                case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+                    ToastAndroid.show(
+                        t('play.services.available'),
+                        ToastAndroid.SHORT
+                    )
+                    break
+                default:
+                    break
+            }
+        } finally {
+            setIsAuthenticating(false)
+        }
+    }
+
+    const signOut = async () => {
+        try {
+            await GoogleSignin.signOut()
+            setUser({})
+            setAccessToken('')
+            setIsSignedIn(false)
+        } catch (error) {
+            // Handle error
+        }
+    }
+
+    const signInSilently = async () => {
+        try {
+            const { data, type } = await GoogleSignin.signInSilently()
+
+            if (type === 'success') {
+                setUser(data.user)
+                setIsSignedIn(true)
+            }
+        } catch (error) {
+            // handle errror
+        }
+    }
+
+    const getIsSignedIn = async () => {
+        try {
+            return await GoogleSignin.isSignedIn()
+        } catch (error) {
+            return false
+        }
+    }
+
+    const getCurrentUser = async () => {
+        try {
+            const { user } = await GoogleSignin.getCurrentUser()
+            return user
+        } catch (error) {
+            return null
+        }
+    }
+
+    const getAccessToken = async () => {
+        try {
+            const { accessToken } = await GoogleSignin.getTokens()
+            return accessToken
+        } catch (error) {
+            return null
+        }
+    }
+
+    useEffect(() => {
+        (async () => {
+            if (!isSignedIn) return
+            setIsAuthenticating(true)
+
+            const currentUser = await getCurrentUser()
+            setUser(currentUser)
+
+            const accessToken = await getAccessToken()
+            setAccessToken(accessToken)
+
+            setIsAuthenticating(false)
+        })()
+    }, [])
+
+    return {
+        user,
+        isSignedIn,
+        setIsSignedIn,
+        signIn,
+        signOut,
+        signInSilently,
+        getIsSignedIn,
+        getAccessToken,
+        getCurrentUser
+    }
+}
